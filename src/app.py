2a731498026275ab994a6397351ed8762729b265
pingwin03 <pingwin03@gmail.com>
2026-08-08 11:27:08 +0200

    Naprawa formatowania pliku app.py

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
-def main():
-    print("Aplikacja działa poprawnie")
+﻿def main():
+    print('Ok')
 
-# Tutaj muszą być dokładnie dwie puste linie powyżej
-if __name__ == "__main__":
-    main() 
-# Pamiętaj też, aby na samym końcu tego pliku (po ostatniej linijce kodu) wcisnąć jeszcze raz Enter, zostawiając pustą linię.
+
+if __name__ == '__main__':
+    main()
+
