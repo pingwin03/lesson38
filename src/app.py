9f33fa54ed48f58df0c5de15fdab7cd1a5b8e063
pingwin03 <pingwin03@gmail.com>
2026-08-08 11:18:48 +0200

    Naprawa wcięć i nowej linii w src/app.py dla flake8

--- src/app.py
+++ src/app.py
@@ -1,8 +1,8 @@
 def main():
     print("Aplikacja działa poprawnie")
 
-
 # Tutaj muszą być dokładnie dwie puste linie powyżej
 if __name__ == "__main__":
     main()
+    
 # Pamiętaj też, aby na samym końcu tego pliku (po ostatniej linijce kodu) wcisnąć jeszcze raz Enter, zostawiając pustą linię.
